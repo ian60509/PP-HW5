ae64237e15c7edd5fa00b5840500dd51958857b7
ian60509 <ian.60509@gmail.com>
2023-12-15 19:48:32 +0800

    Finish part1,2,3

---
+++ src/kernel.cu
@@ -0,0 +1,36 @@
+#include <cuda.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static inline int mandel(float c_re, float c_im, int count)
+{
+  float z_re = c_re, z_im = c_im; //re: real part,  im: imaginary part
+  int i;
+  for (i = 0; i < count; ++i)
+  {
+
+    if (z_re * z_re + z_im * z_im > 4.f)
+      break;
+
+    float new_re = z_re * z_re - z_im * z_im;
+    float new_im = 2.f * z_re * z_im;
+    z_re = c_re + new_re;
+    z_im = c_im + new_im;
+  }
+
+  return i;
+}
+
+__global__ void mandelKernel() {
+    // To avoid error caused by the floating number, use the following pseudo code
+    //
+    // float x = lowerX + thisX * stepX;
+    // float y = lowerY + thisY * stepY;
+}
+
+// Host front-end function that allocates the memory and launches the GPU kernel
+void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, int resX, int resY, int maxIterations)
+{
+    float stepX = (upperX - lowerX) / resX; 
+    float stepY = (upperY - lowerY) / resY;
+}
